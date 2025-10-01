edf11e26319071e58afcbc3f3e30afa7a6a3c3be
tmba <ut42.nu@gmail.com>
2025-10-02 03:29:50 +0900

    fix: unused "Banner"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
-import { Banner, Head } from "nextra/components";
+import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
